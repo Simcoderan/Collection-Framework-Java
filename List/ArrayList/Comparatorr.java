8c43413254296c006bcf2d131fea7bbd241a6bbf
simran <simransingh956429@gmail.com>
2025-01-10 16:34:27 +0530

    linkedlist

--- List/ArrayList/Comparatorr.java
+++ List/ArrayList/Comparatorr.java
@@ -69,3 +69,6 @@ public class Comparatorr {
         
     }
 }
+
+
+//Collections.sort(students,comparator);
---
+++ List/LinkedList/Test.java
@@ -0,0 +1,46 @@
+package List.LinkedList;
+
+import java.util.LinkedList;
+
+public class Test {
+    public static void main(String[] args) {
+
+       LinkedList<Integer> linkedList = new LinkedList<>();
+
+       // ADD METHOD 
+       linkedList.add(1);   //linkedlist is a linear ds where each element is seperate obj called node.
+       linkedList.add(2);   //node--- DATA & POINTER
+       linkedList.add(3);
+       //GET METHOD
+       linkedList.get(2); //O(n)
+       linkedList.addLast(4); //O(1)
+       linkedList.addFirst(0); //O(1)
+       linkedList.getLast();
+       linkedList.getFirst();
+       //REMOVE METHOD
+       linkedList.removeIf(x -> x % 2 == 0); //remove the element if it is  satisfying the condition  in the bracket
+
+
+       
+
+
+        
+    }
+
+   
+    
+}
+
+
+//LINKEDLIST
+/*
+   Better Performance Characteristics compare to ArrayList
+
+     -Insertions and Deletion: doesnt require to shift elemtns
+     -Random Access: slower random access
+     -Memory Overhead : memory more
+
+ */
+
+
+ 
